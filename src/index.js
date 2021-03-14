60d87f203cff04d38407397af5b0e771b6d5f489
Nikita Kryzhevich <Alex-55568@mail.ru>
2021-03-14 21:59:40 +0300

    feat: complet task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined || matrix.length === 0) return [];
+
+  const reverser = (item, index) => {
+    let arr = [];
+    index % 2 ? arr = item.reverse() : arr = item;
+    
+    return arr;
+  }
+
+  return matrix.reduce((accum, item, index) => {
+    
+    return accum.concat(reverser(item, index));
+  });
+
 }
